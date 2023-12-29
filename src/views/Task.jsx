474939c6b70b9cb402e40163cc26328c7d38649a
wanglei <2934490605@qq.com>
2023-12-29 22:44:58 +0800

    feat: 引入ant

---
+++ src/views/Task.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Task.less'
+import { Button } from 'antd'
+class Task extends React.Component {
+    render() {
+        return (
+            <>
+                <Button>按钮</Button>
+            </>
+        )
+    }
+}
+export default Task
